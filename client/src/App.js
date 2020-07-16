00b6d42fba26fadae46290b74f8da4299d3cfe64
Silver Snail 57 <cdarkly1392@gmail.com>
2020-07-16 12:59:33 -0400

    Added bookAPI util, and updated search & saved pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
+
 import Search from "./pages/Search.jsx";
 import Saved from "./pages/Saved.jsx";
 
